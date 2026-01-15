8dd0a9d7e321f0ba4288788f4c5c9a4ed18d98eb
sohan paliyal <--add>
2026-01-15 23:09:45 +0530

    style(sections): update about text and expand layout width for better readability

--- src/components/sections/works.tsx
+++ src/components/sections/works.tsx
@@ -19,7 +19,7 @@ const Works = () => {
             <div className='w-full flex'>
                 <motion.p
                     variants={fadeIn("", "tween", 0.1, 1)}
-                    className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
+                    className='mt-3 text-secondary text-[17px] max-w-full leading-[30px]'
                 >
                     Following projects showcases my skills and experience through
                     real-world examples of my work. Each project is briefly described with
